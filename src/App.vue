18146c5089857f2db8a572843ca19c264d39567b
Ali Hosseini <seyedalihosseinid@gmail.com>
2020-06-23 08:40:28 +0430

    V-1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <Picker />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Picker from "./components/Picker.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Picker,
+  },
+};
 </script>
 
 <style>
